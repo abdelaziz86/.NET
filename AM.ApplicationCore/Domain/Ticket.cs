bcfd2294043c09a1fbb68d5751bc82c591637db3
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-03-06 12:13:58 +0100

    foreign key

--- AM.ApplicationCore/Domain/Ticket.cs
+++ AM.ApplicationCore/Domain/Ticket.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,9 @@ namespace AM.ApplicationCore.Domain
 {
     public class Ticket
     {
-        [Key]
+        [ForeignKey("Passenger")]
         public int PassengerFk { get; set; }
-        [Key]
+        [ForeignKey("Flight")]
         public int FlightFk { get; set; }
         public double Prix { get; set; }
         public string Siege { get; set; }
---
+++ AM.Infrastructure/Migrations/20230306111327_TPT2à.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TPT2à : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Passenger_PassengerPassportNumber",
+                table: "Ticket");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Vols_FlightId",
+                table: "Ticket");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ticket_FlightId",
+                table: "Ticket");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ticket_PassengerPassportNumber",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "FlightId",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "PassengerPassportNumber",
+                table: "Ticket");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_PassengerFk",
+                table: "Ticket",
+                column: "PassengerFk");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Passenger_PassengerFk",
+                table: "Ticket",
+                column: "PassengerFk",
+                principalTable: "Passenger",
+                principalColumn: "PassportNumber",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Vols_FlightFk",
+                table: "Ticket",
+                column: "FlightFk",
+                principalTable: "Vols",
+                principalColumn: "FlightId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Passenger_PassengerFk",
+                table: "Ticket");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Vols_FlightFk",
+                table: "Ticket");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ticket_PassengerFk",
+                table: "Ticket");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FlightId",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PassengerPassportNumber",
+                table: "Ticket",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_FlightId",
+                table: "Ticket",
+                column: "FlightId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_PassengerPassportNumber",
+                table: "Ticket",
+                column: "PassengerPassportNumber");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Passenger_PassengerPassportNumber",
+                table: "Ticket",
+                column: "PassengerPassportNumber",
+                principalTable: "Passenger",
+                principalColumn: "PassportNumber",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Vols_FlightId",
+                table: "Ticket",
+                column: "FlightId",
+                principalTable: "Vols",
+                principalColumn: "FlightId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
